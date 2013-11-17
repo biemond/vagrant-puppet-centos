77d9138a8ff6d204df2a272f4c3b606d335b0e62
Edwin Biemond <biemond@gmail.com>
2013-11-17 22:33:07 +0100

    added hiera example with lambda examples

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -4,8 +4,50 @@
 
 node 'admin2.example.com' {
 
-  $msg = hiera('messageEnv')
-  notify { "message ${msg}" :} 
+    $msg = hiera('messageEnv')
+    notice "message ${msg}" 
+
+    # read all server entries from hiera yaml
+    $entries = hiera_array('servers')
+
+    $entries.each |$index5,$value5| { 
+        notice "----"
+        notice $index5
+
+        $entries[$index5].each |$index6,$value6| {
+            #global params
+            notice $index6
+            $value6['global_parameters'].each |$index,$value| { 
+                if ( $index != 'params') {
+                    notice "global $index $value" 
+                } else {
+                    # params 
+                    $parameters = $value 
+                    $parameters.each |$value2| { 
+                        notice "global parameters $value2" 
+                    }
+                }
+            }          
+
+            $value6['entries'].each |$index,$value| { 
+                $value.each |$index2,$value2| {
+                    if ( $index2 != 'params') {
+                        notice "entry $index2 $value2" 
+                    } else {
+                        # params 
+                        $parameters = $value2 
+                        $parameters.each |$value3| { 
+                            notice "entry parameters $value3" 
+                        }
+                    }
+                }
+            } 
+
+
+        }
+
+    } 
+    notice "----"
 
 }
 
